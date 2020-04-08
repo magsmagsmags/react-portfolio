41fb2b48dcb52e9c4ce42d940e2a3fc1bd8185e8
Maggie Kiefer <57575667+magsmagsmags@users.noreply.github.com>
2020-04-08 17:26:54 -0500

    content updates to aboutme

--- src/components/resume.js
+++ src/components/resume.js
@@ -13,7 +13,7 @@ class Resume extends Component {
                     <Cell col={4}>
                         <div style={{ textAlign: 'center' }}>
                             <img
-                                src="./avatar1.png"
+                                src="./headshot.jpg"
                                 alt="avatar"
                                 style={{ height: '250px' }}
                             />
@@ -21,9 +21,9 @@ class Resume extends Component {
 
                         <h2 style={{ paddingTop: '2em' }}>Mags Kiefer</h2>
                         <h4 style={{ color: 'grey' }}>Project Manager and Developer</h4>
-                        <hr style={{ borderTop: '3px solid #833fb2', width: '50%' }} />
+                        <hr style={{ borderTop: '5px solid #6f697d', width: '50%' }} />
                         <p>My name is Mags Kiefer. I'm an experienced Project Manager, Product Manager, a former Chief of Staff for a SaaS company, and a Full Stack Developer. Located in Austin, Texas and open to local and remote opportunities!</p>
-                        <hr style={{ borderTop: '3px solid #833fb2', width: '50%' }} />
+                        <hr style={{ borderTop: '5px solid #6f697d', width: '50%' }} />
                         <h5>Location</h5>
                         <p>Austin, TX </p>
                         <h5>Phone</h5>
@@ -32,7 +32,7 @@ class Resume extends Component {
                         <p>sheakiefer@gmail.com</p>
                         <h5>Web</h5>
                         <p>https://www.linkedin.com/in/sheakiefer/</p>
-                        <hr style={{ borderTop: '3px solid #833fb2', width: '50%' }} />
+                        <hr style={{ borderTop: '5px solid #6f697d', width: '50%' }} />
                     </Cell>
                     <Cell className="resume-right-col" col={8}>
                         <h2>Education</h2>
@@ -48,18 +48,25 @@ class Resume extends Component {
                         <Education
                             startYear={2019}
                             endYear={2020}
-                            schoolName="UT Austin Coding Bootcamp"
+                            schoolName="University of Texas at Austin"
                             schoolDescription="Certification in skills and competencies required to be a Full Stack Developer"
                         />
-                        <hr style={{ borderTop: '3px solid #e22947' }} />
+                        <hr style={{ borderTop: '5px solid #6f697d' }} />
 
                         <h2>Experience</h2>
 
                         <Experience
                             startYear={2016}
                             endYear={2017}
+                            jobName="Full Stack Developer"
+                            jobDescription="Bright Light Health || A health-focused small business in need of a website for their newly launching podcast"
+                        />
+
+                        <Experience
+                            startYear={2016}
+                            endYear={2017}
                             jobName="Chief of Staff; Project Manager"
-                            jobDescription="NectarOM || A SaaS company in Dallas, Texas"
+                            jobDescription="NectarOM || A SaaS company in Dallas, Texas developing B2B marketing personalization and automation tools"
                         />
 
                         <Experience
@@ -68,7 +75,7 @@ class Resume extends Component {
                             jobName="Client Services and Sales Associate"
                             jobDescription="Blue Star Payment Solutions || A Payment Processing Company that launched a web and mobile app for a nonprofit donations."
                         />
-                        <hr style={{ borderTop: '3px solid #e22947' }} />
+                        <hr style={{ borderTop: '5px solid #6f697d' }} />
                         <h2>Skills</h2>
                         <Skills
                             skill="javascript"
